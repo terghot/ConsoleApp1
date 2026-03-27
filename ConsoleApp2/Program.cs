b4929b03e9b89b6ebd51dbb253b03287f8fd17c7
terghot <artemvzlom666@mail.ru>
2026-03-27 16:05:22 +0300

    addConfig

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-<<<<<<< HEAD
+
 
 Console.WriteLine("DataBase1");
 
@@ -9,8 +9,9 @@ Console.WriteLine("DataBase1");
 Console.WriteLine("DataBase2");
 
 Console.WriteLine("DataBase3");
-=======
+
 Console.WriteLine("File.1");
 
 Console.WriteLine("File.2");
->>>>>>> master
+
+Console.WriteLine("addConfig");
